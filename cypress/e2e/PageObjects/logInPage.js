272ea1501063aed832b2150507e4cb0fc0a7b2a6
AndreZegarra <136137641+AndreZegarra@users.noreply.github.com>
2023-06-10 12:42:36 -0500

    Add files via upload

---
+++ cypress/e2e/PageObjects/logInPage.js
@@ -0,0 +1,46 @@
+const logoImg = 'a > img';
+const loginLnk = '.shop-menu > .nav > :nth-child(4) > a';
+const emailTxt = '.login-form > form > [type="email"]';
+const passwordTxt = '[type="password"]';
+const loginBtn = '.login-form > form > .btn';
+const loggedInLbl = ':nth-child(9) > a';
+const incorrectEmailLbl = '.login-form > form > p';
+
+class logInPage {
+  static homepage() {
+    cy.visit(`${Cypress.env('baseUrl')}`);
+    cy.get(logoImg).should('be.visible');
+  }
+
+  static navigateToLogin() {
+    cy.get(loginLnk).click();
+  }
+
+  static enterUserCreds() {
+    cy.get(emailTxt).type(`${Cypress.env('QaAutomationUser')}`);
+    cy.get(passwordTxt).type(`${Cypress.env('QaAutomationPassword')}`);
+  }
+
+  static enterInvalidCreds() {
+    cy.get(emailTxt).type('usario_invalido@email.com');
+    cy.get(passwordTxt).type('usuarioinvalido');
+  }
+
+  static clickLoginButton() {
+    cy.get(loginBtn).click();
+  }
+
+  static validateUserIsLoggedIn() {
+    cy.get(loggedInLbl).should('be.visible');
+  }
+
+  static validateIncorrectLogin() {
+    cy.get(incorrectEmailLbl).should('be.visible');
+  }
+
+  static validateLoginPageIsDisplayed() {
+    cy.get(emailTxt).should('be.visible');
+  }
+}
+
+export default logInPage;
